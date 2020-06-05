cb2de83b003648e694dfd502d980522bee0e72cc
Parker Williamson <parker.williamson@gmail.com>
2020-06-05 18:28:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,28 +3,6 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-  
-  def self.table_name
-    self.to_s.downcase.pluralize
-  end
-  
-  def self.column_names
-    DB[:conn].results_as_hash = true 
-    
-    sql = "pragma table_info('#{table_name}')"
-    
-    table_info = DB[:conn].execute(sql)
-    column_names = [] 
-    table_info.each do |row|
-      column_names << row["name"]
-    end
-    column_names.compact
-  end
-  
-  def initialize(options={})
-    options.each do |property, value|
-      self.send("#{property}=", value)
-    end
-  end
+
 
 end
